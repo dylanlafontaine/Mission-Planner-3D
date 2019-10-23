12f7e3b602659a91360966217140fc39609639d9
warior4356 <warior4356@yahoo.com>
2019-10-22 18:55:07 -0700

    Added ball creation and test movement

---
+++ Assets/ball.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // This object was clicked - do something
+    void OnMouseDown()
+    {
+        var rand = new System.Random();
+        transform.position = transform.position + new Vector3(rand.Next(3), rand.Next(3), rand.Next(3));
+    }
+}
---
+++ Assets/readSpawnTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class readSpawnTest : MonoBehaviour
+{
+    public GameObject myPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string line;
+        int[] nums = new int[4];
+        // Read the file and display it line by line.  
+        System.IO.StreamReader file =
+            new System.IO.StreamReader(@"points.txt");
+        while ((line = file.ReadLine()) != null)
+        {
+            string[] words = line.Split(' ');
+            for(int i = 0; i < 3; i++)
+            {
+                //Debug.Log(words[i]);
+                nums[i] = Int32.Parse(words[i]);
+                //Debug.Log(nums[i]);
+            }
+            Instantiate(myPrefab, new Vector3(nums[1], nums[2], nums[3]), Quaternion.identity);
+            //Debug.Log(line);
+        }
+        Debug.Log("Up and running");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
