63b9b7cf403989542796b109f9da01a5c6c19933
warior4356 <warior4356@yahoo.com>
2019-11-14 18:34:42 -0800

    Moved import to import button

---
+++ Assets/MasterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterController : MonoBehaviour
+{
+    public List<GameObject> points = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LineRenderer line;
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            line = points[i].GetComponent(typeof(LineRenderer)) as LineRenderer;
+            line.SetPosition(0, points[i].transform.position);
+            line.SetPosition(1, points[i + 1].transform.position);
+            line.startWidth = 5f;
+            line.endWidth = .1f;
+        }
+    }
+}
