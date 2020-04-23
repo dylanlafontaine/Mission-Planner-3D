53e2ef1be87c5353f684cb1a28f155549da7ac95
Ryal <warior4356@yahoo.com>
2020-04-23 01:50:10 -0700

    Adding CI handler

---
+++ Assets/CI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+class CIScript
+{
+     static void PerformBuild ()
+     {
+		 BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+         buildPlayerOptions.scenes = new[] { "Assets/maptest.unity" };
+         buildPlayerOptions.locationPathName = "winBuild";
+         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+         buildPlayerOptions.options = BuildOptions.None;
+         BuildPipeline.BuildPlayer(buildPlayerOptions);
+     }
+}
