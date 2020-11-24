4a7401844da5056f457b9d62a00d8633a0c763b2
dlafonta <dlafonta@eecs.wsu.edu>
2020-11-23 18:11:47 -0800

    Missed ball.cs so that is now Ball.cs

--- Assets/ball.cs
+++ Assets/ball.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class ball : MonoBehaviour
 {
     // This object was clicked - do something
     void OnMouseDown()
