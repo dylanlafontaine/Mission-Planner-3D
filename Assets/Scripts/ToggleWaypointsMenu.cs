d07f0eb76bbfa3536ce84959ce76569fad158ca9
dlafonta <dlafonta@eecs.wsu.edu>
2020-11-27 00:07:07 -0800

    The user can now open and close the waypoint menu as they wish with a toggle button.

---
+++ Assets/Scripts/ToggleWaypointsMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleWaypointsMenu : MonoBehaviour
+{
+    public GameObject waypointsMenu;
+    public Button toggleButton;
+
+    public void HandleMenuToggle()
+    {
+
+        if (waypointsMenu.activeSelf == true)
+        {
+            waypointsMenu.SetActive(false);
+            toggleButton.image.transform.Rotate(0, 0, 180);
+        }
+        else
+        {
+            waypointsMenu.SetActive(true);
+            toggleButton.image.transform.Rotate(0, 0, 180);
+        }
+    }
+}
